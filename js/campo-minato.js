56798d5c605a8063068785d116bb247500fe4be8
Giulio <giuliovolletti@hotmail.com>
2020-11-20 16:10:49 +0100

    commit 4

--- js/campo-minato.js
+++ js/campo-minato.js
@@ -1,10 +1,27 @@
 console.log("hello world");
 
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+var mineDifficolta = 0;
+var difficoltà = parseInt(prompt("scegli un livello di difficoltà tra 0 / 1 / 2"))
+switch (difficoltà) {
+  case 1:
+      mineDifficolta = 80;
+    break;
+  case 2:
+    mineDifficolta = 50;
+    break;
+  default:
+  mineDifficolta = 100;
+}
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 var arrayNumeri = [];
 
 while (arrayNumeri.length < 16) {
-  var numeroCaso = numeriCasuali(100, 1);
+  var numeroCaso = numeriCasuali(mineDifficolta, 1);
   var verificaDati = verifica(numeroCaso, arrayNumeri);
   // I numeri non possono essere duplicati.
   if (verificaDati == false) {
@@ -16,9 +33,10 @@ while (arrayNumeri.length < 16) {
 console.log(arrayNumeri.sort());
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 var arrayUtente = [];
-var richiestaNumeri = 100 - 16;
+var richiestaNumeri = mineDifficolta - 16;
 // test
 // richiestaNumeri = 5;
+
 var perso = false;
 while (arrayUtente.length < richiestaNumeri && perso != true) {
   var numeriUtente = parseInt(prompt("inserisci numeri"));
@@ -31,8 +49,8 @@ while (arrayUtente.length < richiestaNumeri && perso != true) {
   }
 }
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 alert("punteggio: " + arrayUtente.length)
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 console.log(arrayUtente.sort());
 console.log("punteggio: ", arrayUtente.length);
 
